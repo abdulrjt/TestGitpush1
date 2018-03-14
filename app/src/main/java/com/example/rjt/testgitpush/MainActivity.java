d4bb8d37383de9fb75bbb7998a1444b4cc78ab51
abdulrjt <abdul@rjtcompuquest.com>
2018-03-14 14:48:45 -0500

    Initial commit

---
+++ app/src/main/java/com/example/rjt/testgitpush/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.rjt.testgitpush;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    private void testupload(){
+        //checking if file is uploaded to github..
+    }
+}
